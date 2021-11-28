a39a12e805e310a9bac688bb4ade91f53d10842d
Michal Grzybowski <michalgrzybowski4@gmail.com>
2021-11-28 20:51:47 +0100

    Children nodes + better root node (part 3)

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main() {
 	XMLDocument doc;
 	if(XMLDocument_load(&doc, FILE_PATH)) {
-		XMLNode* more_node = XMLNode_child(XMLNode_child(doc.root, 0), 0);
-		printf("%s: %s\n", more_node->tag, more_node->inner_text);
+		XMLNode* main_node = XMLNode_child(doc.root, 0);
+		printf("%d children\n", main_node->children.size);
 
 		XMLNode* another_node = XMLNode_child(doc.root, 1);
 		printf("%s: %s\n", another_node->tag, another_node->inner_text);
